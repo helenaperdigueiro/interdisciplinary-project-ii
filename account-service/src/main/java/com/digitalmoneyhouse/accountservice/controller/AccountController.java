313fd627c09f479d8e01b6236f4f80b147b073f3
cahak <haka.carolina@gmail.com>
2023-02-28 20:28:39 -0300

    Exception handling and find by id

--- account-service/src/main/java/com/digitalmoneyhouse/accountservice/controller/AccountController.java
+++ account-service/src/main/java/com/digitalmoneyhouse/accountservice/controller/AccountController.java
@@ -1,14 +1,12 @@
 package com.digitalmoneyhouse.accountservice.controller;
 
+import com.digitalmoneyhouse.accountservice.exception.BusinessException;
 import com.digitalmoneyhouse.accountservice.model.Account;
 import com.digitalmoneyhouse.accountservice.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/accounts")
@@ -20,5 +18,10 @@ public class AccountController {
     @PostMapping()
     public ResponseEntity<Account> save(@RequestBody Account account) {
         return ResponseEntity.status(HttpStatus.CREATED).body(accountService.save(account));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Account> findById(@PathVariable Integer id) throws BusinessException {
+        return ResponseEntity.status(HttpStatus.OK).body(accountService.findById(id));
     }
 }
--- account-service/src/main/java/com/digitalmoneyhouse/accountservice/service/AccountService.java
+++ account-service/src/main/java/com/digitalmoneyhouse/accountservice/service/AccountService.java
@@ -1,10 +1,12 @@
 package com.digitalmoneyhouse.accountservice.service;
 
+import com.digitalmoneyhouse.accountservice.exception.AccountNotFoundException;
 import com.digitalmoneyhouse.accountservice.model.Account;
 import com.digitalmoneyhouse.accountservice.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.Random;
 
 @Service
@@ -28,5 +30,10 @@ public class AccountService {
     public String generateAccountNumber() {
         Random generator = new Random();
         return String.valueOf(generator.nextInt(100000,1000000));
+    }
+
+    public Account findById(Integer id) throws AccountNotFoundException {
+        Account account = accountRepository.findById(id).orElseThrow(AccountNotFoundException::new);
+        return account;
     }
 }
