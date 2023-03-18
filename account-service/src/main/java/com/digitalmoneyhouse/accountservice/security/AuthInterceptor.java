9ea5eff795c9cbae99d9e60035c113a36a88f42b
Bruno Barbosa <brb.dev@outlook.com>
2023-03-18 18:14:17 -0400

    improves security access

--- account-service/src/main/java/com/digitalmoneyhouse/accountservice/security/AuthInterceptor.java
+++ account-service/src/main/java/com/digitalmoneyhouse/accountservice/security/AuthInterceptor.java
@@ -1,7 +1,9 @@
 package com.digitalmoneyhouse.accountservice.security;
 
 import com.digitalmoneyhouse.accountservice.exception.AuthorizationHeaderNotFoundException;
+import com.digitalmoneyhouse.accountservice.exception.BusinessException;
 import com.digitalmoneyhouse.accountservice.exception.InvalidTokenException;
+import com.digitalmoneyhouse.accountservice.service.AccountService;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,11 +11,21 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AuthInterceptor implements HandlerInterceptor {
+
+    private AccountService accountService;
+
+    public AuthInterceptor(AccountService accountService) {
+        this.accountService = accountService;
+    }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -27,6 +39,15 @@ public class AuthInterceptor implements HandlerInterceptor {
             throw new AuthorizationHeaderNotFoundException();
         }
         token = token.substring(7);
+
+        String[] chunks = token.split("\\.");
+        String payload = new String(Base64.getDecoder().decode(chunks[1]));
+        Integer userIdFromToken = Integer.valueOf(stringPayloadToMap(payload).get("userId"));
+        Integer userIdByAccountPathId = accountService.findById(getAccountIdFromPath(requestUrl)).getUserId();
+        if (userIdByAccountPathId != userIdFromToken) {
+            throw new BusinessException(403, "Unauthorized");
+        }
+
         URI url = new URI("http://localhost:8080/validate-token");
         Gson gson = new Gson();
         JsonObject body = new JsonObject();
@@ -45,5 +66,31 @@ public class AuthInterceptor implements HandlerInterceptor {
         } else {
             throw new InvalidTokenException();
         }
+    }
+
+    private Integer getAccountIdFromPath(String requestUrl) {
+        try {
+            URI uri = new URI(requestUrl);
+            String[] segments = uri.getPath().split("/");
+            return Integer.valueOf(segments[2]);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Map<String, String> stringPayloadToMap(String payload) {
+        Map<String, String> payloadMap = new HashMap<>();
+        String[] payloadList = payload
+                .replace("\"", "")
+                .replace("{", "")
+                .replace("}", "")
+                .split(",");
+        for (String item : payloadList) {
+            String[] keyAndValue = item.split(":");
+            String key = keyAndValue[0];
+            String value = keyAndValue[1];
+            payloadMap.put(key, value);
+        }
+        return payloadMap;
     }
 }
